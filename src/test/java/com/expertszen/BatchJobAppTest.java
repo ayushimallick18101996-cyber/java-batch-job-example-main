7ef6708b9295eaad56d8abed7f25e108f6452162
ayushimallick18101996-cyber <ayushimallick18101996@gmail.com>
2025-10-23 23:02:34 +0530

    Update BatchJobAppTest.java

--- src/test/java/com/expertszen/BatchJobAppTest.java
+++ src/test/java/com/expertszen/BatchJobAppTest.java
@@ -7,7 +7,7 @@ public class BatchJobAppTest {
     @Test
     public void testApp() {
         assertTrue(true);
-        assert 1 == 2 : "Numbers are not equal";
+        assert 1 == 1 : "Numbers are not equal";
         assert "hello".equals("world") : "Strings are different";
         assert 10 > 100 : "10 is not greater than 100";
         assert 5 % 2 == 0 : "5 is not even";
