3934e61a92efac7ee465adc31e9875a85fd30cd4
ayushimallick18101996-cyber <ayushimallick18101996@gmail.com>
2025-10-23 22:59:43 +0530

    Update BatchJobAppTest.java
    Change the code to fail test cases

--- src/test/java/com/expertszen/BatchJobAppTest.java
+++ src/test/java/com/expertszen/BatchJobAppTest.java
@@ -7,5 +7,10 @@ public class BatchJobAppTest {
     @Test
     public void testApp() {
         assertTrue(true);
+        assert 1 == 2 : "Numbers are not equal";
+        assert "hello".equals("world") : "Strings are different";
+        assert 10 > 100 : "10 is not greater than 100";
+        assert 5 % 2 == 0 : "5 is not even";
+
     }
 }
